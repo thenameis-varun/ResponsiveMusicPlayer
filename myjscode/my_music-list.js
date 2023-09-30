afc42f87c66aa24c10fc668c9e3aa44f05165be8
itsSimplyVarun <119106180+thenameis-varun@users.noreply.github.com>
2023-09-30 17:58:14 +0530

    Create my_music-list.js

---
+++ myjscode/my_music-list.js
@@ -0,0 +1,181 @@
+let allMusic = [
+
+    {
+        name: "Man on the moon",
+        artist: "Alan Walker & Ingrosso",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Astronaut In The Ocean",
+        artist: "MASKED WOLF",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "STAY",
+        artist: "Justin Bieber, The Kid LAROI",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "edamame",
+        artist: "bbno$",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Music for a Sushi Restaurant",
+        artist: "Harry Styles",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "MONTERO",
+        artist: "Lil Nas X",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Starboy",
+        artist: "The Weeknd",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Stitches",
+        artist: "Shawn Mendes",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "August Diaries",
+        artist: "Dharia",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Goodness Gracious",
+        artist: "Yung Gravy, bbno$",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "THATS WHAT I WANT",
+        artist: "Lil Nas X",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Cure for me",
+        artist: "Aurora",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Watermelon Sugar",
+        artist: "Harry Styles",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Counting Stars",
+        artist: "OneRepublic",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "All Time Low",
+        artist: "jon bellion",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "PEOPLE",
+        artist: "LIBIANCA",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Don't start now",
+        artist: "DUA LIPA",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Lily",
+        artist: "Alan Walker",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name: "Falling",
+        artist: "trevor daniel",
+        img: "music-19",
+        src: "music-19"
+    },
+    {
+        name: "IN THE STARS",
+        artist: "Benson boone",
+        img: "music-20",
+        src: "music-20"
+    },
+    {
+        name: "Harleys In Hawaii",
+        artist: "Katy Perry, KANDY",
+        img: "music-21",
+        src: "music-21"
+    },
+    {
+        name: "I Ain't Worried",
+        artist: "OneRepublic",
+        img: "music-22",
+        src: "music-22"
+    },
+    
+    {
+        name: "Daylight",
+        artist: "David Kushner",
+        img: "music-23",
+        src: "music-23"
+    },
+    
+    {
+        name: "INFERNO",
+        artist: "Bella Poarch, Sub Urban",
+        img: "music-24",
+        src: "music-24"
+    },
+    
+    {
+        name: "Lush Life",
+        artist: "zara larsson",
+        img: "music-25",
+        src: "music-25"
+    },
+    
+    {
+        name: "Heat Waves",
+        artist: "Glass Animals",
+        img: "music-26",
+        src: "music-26"
+    },
+    
+    {
+        name: "Arcade",
+        artist: "Duncan Laurence",
+        img: "music-27",
+        src: "music-27"
+    },
+    
+    {
+        name: "Payphone",
+        artist: "MAROON 5",
+        img: "music-28",
+        src: "music-28"
+    },
+    
+    ];
+  
+    
+    
